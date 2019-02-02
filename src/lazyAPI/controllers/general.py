cf222bb3a9dae056672792ecbac5a5d9faac5fba
dmium <dominiczy.hall@gmail.com>
2019-02-02 19:47:13 +0000

    Added general methods for getting project names and types

---
+++ src/lazyAPI/controllers/general.py
@@ -0,0 +1,18 @@
+from lazyAPI import app, mongo
+from flask import jsonify, request, Response
+from bson import Binary, Code
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+
+def get_projects():
+    project = []
+    for coll in mongo.db.collection_names():
+        project.append(coll.split('/')[0])
+    return project
+
+def get_types(project):
+    types = []
+    for coll in mongo.db.collection_names():
+        if coll.startswith(project):
+            types.append(coll.split('/')[1])
+    return types
