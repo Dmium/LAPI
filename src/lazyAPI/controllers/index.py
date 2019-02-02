07d2d6ba5e9aed1c335743b470c980172259c4ad
dmium <dominiczy.hall@gmail.com>
2019-02-02 15:46:52 +0000

    We changed our mind about slashes

--- src/lazyAPI/controllers/index.py
+++ src/lazyAPI/controllers/index.py
@@ -12,18 +12,18 @@ def init_database(project):
             mongo.db[coll].drop()
     return 'Init complete'
 
-@app.route('/<project>/<type>/', methods=['POST'])
+@app.route('/<project>/<type>', methods=['POST'])
 def create(project, type):
     request_dict = request.get_json()
     newobjid = mongo.db[str(project) + '/' + str(type)].insert_one(request_dict).inserted_id
     return Response(dumps(mongo.db[str(project) + '/' + str(type)].find_one({"_id": newobjid})), status=200, mimetype='application/json')
 
-@app.route('/<project>/<type>/<oid>/', methods=['GET'])
+@app.route('/<project>/<type>/<oid>', methods=['GET'])
 def read(project, type, oid):
     print(oid)
     return Response(dumps(mongo.db[str(project) + '/' + str(type)].find_one({"_id": ObjectId(str(oid))})), status=200, mimetype='application/json')
 
-@app.route('/<project>/<type>/', methods=['GET'])
+@app.route('/<project>/<type>', methods=['GET'])
 def read_all(project, type):
     request_dict = request.get_json()
     if(request_dict == None):
@@ -31,13 +31,13 @@ def read_all(project, type):
     else:
         return Response(dumps(mongo.db[str(project) + '/' + str(type)].find(request_dict)), status=200, mimetype='application/json')
 
-@app.route('/<project>/<type>/<oid>/', methods=['PUT'])
+@app.route('/<project>/<type>/<oid>', methods=['PUT'])
 def update(project, type, oid): # replace appropriate fields
     request_dict = request.get_json()
     mongo.db[str(project) + '/' + str(type)].update_one({'_id':ObjectId(str(oid))}, {"$set": request_dict})
     return Response(dumps(mongo.db[str(project) + '/' + str(type)].find_one({"_id": ObjectId(str(oid))})), status=200, mimetype='application/json')
 
-@app.route('/<project>/<type>/<oid>/', methods=['DELETE'])
+@app.route('/<project>/<type>/<oid>', methods=['DELETE'])
 def delete(project, type, oid):
     mongo.db[str(project) + '/' + str(type)].delete_one({'_id':ObjectId(str(oid))})
     return Response("", status=200, mimetype='application/json')
