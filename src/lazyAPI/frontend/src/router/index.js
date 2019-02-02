9f1726f1378a96595c406ecb7d7424a93da322aa
James Williams <jameswilliams150598@gmail.com>
2019-02-02 16:41:11 +0000

    Add 404 page

--- src/lazyAPI/frontend/src/router/index.js
+++ src/lazyAPI/frontend/src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 
 const routerOptions = [
   { path: '/', component: 'Home' },
-  { path: '/about', component: 'About' }
+  { path: '*', component: 'NotFound' }
 ]
 
 const routes = routerOptions.map(route => {
