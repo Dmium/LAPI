b20cf0d6c2ece3a07ebaffa7af1bb68371615e13
dmium <dominiczy.hall@gmail.com>
2019-02-03 01:37:18 +0000

    Added generating controllers and fixed some models

--- src/lazyAPI/controllers/generate.py
+++ src/lazyAPI/controllers/generate.py
@@ -6,7 +6,7 @@ from bson.json_util import dumps
 import os
 import shutil
 
-@app.route('/generate/<project>', methods=['POST'])
+@app.route('/generate/<project>', methods=['GET'])
 def generate(project):
     path = "../projects/src/" + project + "/"
     if(os.path.exists(path)):
@@ -20,8 +20,7 @@ def generate(project):
         if coll.startswith(project):
             modelname = coll[(coll.find("/")+1):]
             f = open(path + project + "/models/" + modelname + ".py", "w+")
-            f.write("from " + project + " import db\n")
-            f.write("from " + project + " import ma\n\n")
+            f.write("from " + project + " import db, ma\n\n")
             f.write("class " + modelname.title() + "(db.Model):\n")
             f.write("    __tablename__ = \"" + modelname + "\"\n")
             f.write("    id = db.Column(db.Integer, primary_key = True)\n")
@@ -47,4 +46,26 @@ def generate(project):
             f.write("\n        fields = ('id',)\n\n")
             f.write(modelname + "_schema" + " = " + modelname.title() + "_Schema()\n")
             f.write(modelname + "_schemas" + " = " + modelname.title() + "_Schema(many=True)\n")
+            f.close()
+    generate_controllers(project)
     return "ok"
+
+def generate_controllers(project):
+    path = "../projects/src/" + project + "/"
+    os.makedirs(path + project + "/controllers/")
+    for coll in mongo.db.collection_names():
+        if coll.startswith(project):
+            modelname = coll[(coll.find("/")+1):]
+            f = open(path + project + "/controllers/" + modelname + ".py", "w+")
+            f.write("from " + project + " import db, ma, app\n")
+            f.write("from flask import jsonify, request, make_response\n")
+            f.write("from " + project + ".models." + modelname + " import " + modelname + ", " + modelname + "_schema, " + modelname + "_schemas\n\n")
+            f.write("@app.route('/api/" + modelname + "', methods=['POST'])\n")
+            f.write("def create_" + modelname + "():\n")
+            f.write("  db.session.add(" + modelname + "(**request.get_json()))\n")
+            f.write("  db.session.commit()\n")
+            f.write("  return jsonify(['ok'])\n\n")
+            f.write("@app.route('/api/" + modelname + "', methods=['GET'])\n")
+            f.write("def read_" + modelname + "():\n")
+            f.write("  return jsonify(" + modelname + ".query.all())\n\n")
+            f.close()
