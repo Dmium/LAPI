9d48ed72748c395706fed73475b55f2026db3c08
Dom Hall <dominiczy.hall@gmail.com>
2019-07-18 16:01:21 +0100

    Automatically generate docs and html on rebuild

--- src/rebuild.sh
+++ src/rebuild.sh
@@ -1,7 +1,16 @@
+# Build front end
 cd lazyAPI/frontend/lapi/
 npm run build
 cd ../../../
+#Prepare flask env variables
 export FLASK_APP=lazyAPI
 export FLASK_ENV=development
+#Install LAPI backend
 pip3 install --user -e .
+#Refresh docs
+cd ../docs
+python index_gen.py
+make html
+cd ../src
+#Start server
 python -m flask run
